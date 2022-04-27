e21d3d896a98a15a63cf24abb8491f10112e9202
David Weiß <davidweisspost@gmail.com>
2022-04-27 21:05:44 +0000

    Style streamlit

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -2,47 +2,27 @@ import streamlit as st
 import deepl
 import lookup_dictionary
 
-# Apply a general css style to a streamlit page
-# NOTE that this injects HTML via the unsafe_allow_html=True parameter
-# def css_styling():
-#     css = """
-#         div[role="radiogroup"] {
-#         border-left: 2px solid grey;
-#         padding-left: 10px;
-#         }
-# 
-#         MainMenu {visibility: hidden; }
-#         footer {visibility: hidden;}
-#     """
-#     st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
-# 
-# css_styling()
+st.set_page_config(
+    layout="wide", page_title="Health Care", page_icon="🚑"
+)  # Use the full page instead of a narrow central column
 
-# with st.sidebar:
-#     # with st.echo():
-#     st.write("This code will be printed to the sidebar.")
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+local_css("styles.css")
 
 #DeepL translator
 auth_key = "bf16c3b7-c877-103a-8e59-08527ae43ef9:fx"  # Replace with your key
 translator = deepl.Translator(auth_key)
 
-st.markdown("""
-    <style>
-    .big-font {
-        font-size:22px !important;
-    }
-    </style>
-    """, unsafe_allow_html=True)
-
+st.sidebar.image("logo.jpg")
 
 navi = st.sidebar.radio('Navigation', options=["Patient Info", "Diagnosis"])
 
 if navi == "Patient Info":
-    # st.markdown('<p class="big-font">Patient Info</p>', unsafe_allow_html=True)
-
     name = st.text_input(label="Name:")
     sex = st.text_input(label="Sex:")
-    # age = st.text_input(label="Age:")
     age = st.slider("Age:", 1, 100)
     size = st.text_input(label="Height:")
     weight = st.text_input(label="Weight:")
