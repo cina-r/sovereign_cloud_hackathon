f5578edc5521547e15ec6e5b460e1c89a13cdf32
FrieMi <friederike.michaelis@telekom.de>
2022-04-27 20:36:42 +0000

    speech recognition

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import deepl
 import lookup_dictionary
+import speech_recognition as sr
 
 # Apply a general css style to a streamlit page
 # NOTE that this injects HTML via the unsafe_allow_html=True parameter
@@ -36,18 +37,39 @@ st.markdown("""
 
 
 navi = st.sidebar.radio('Navigation', options=["Doctor's View", "Patient's View"])
-diagnosis_doc=""
+
+# Initialization
+if 'diagnosis_text' not in st.session_state:
+    st.session_state['diagnosis_text'] = ''
+    st.session_state['age'] = 1
+    st.session_state['name'] = ''
+    st.session_state['sex'] = ''
+    st.session_state['weight'] = ''
+    st.session_state['height'] = ''
+
 
 if navi == "Doctor's View":
     # st.markdown('<p class="big-font">Patient Info</p>', unsafe_allow_html=True)
 
-    name = st.text_input(label="Name:")
-    sex = st.text_input(label="Sex:")
-    # age = st.text_input(label="Age:")
-    age = st.slider("Age:", 1, 100)
-    size = st.text_input(label="Height:")
-    weight = st.text_input(label="Weight:")
+    st.session_state['name'] = st.text_input(label="Name:", value = st.session_state['name'])
+    st.session_state['sex'] = st.text_input(label="Sex:", value = st.session_state['sex'])
+    st.session_state['age'] = st.slider("Age:", 1, 100, value = st.session_state['age'])
+    st.session_state['height'] = st.text_input(label="Height:", value = st.session_state['height'])
+    st.session_state['weight'] = st.text_input(label="Weight:", value = st.session_state['weight'])
     diagnosis_doc = st.selectbox("Diagnosis:", ["audio-test-1.wav","audio-test-2.wav"])
+    audio_file = open(diagnosis_doc, 'rb')
+    audio_bytes = audio_file.read()
+    st.audio(audio_bytes, format='audio/wav')
+    if st.button("Speech2Text"):
+        r = sr.Recognizer()
+        with sr.AudioFile(diagnosis_doc) as source:
+            # listen for the data (load audio to memory)
+            audio_data = r.record(source)
+            # recognize (convert from speech to text)
+            text = r.recognize_sphinx(audio_data)
+            st.session_state['diagnosis_text'] = text
+    st.write(st.session_state['diagnosis_text'])
+
 
     # st.write("")
     # st.markdown('<p class="big-font">Diagnosis</p>', unsafe_allow_html=True)
@@ -64,9 +86,9 @@ if navi == "Patient's View":
     # st.write("")
 
     st.write("Diagnosis explained:")
-    if diagnosis_doc:
+    if st.session_state['diagnosis_text']:
         medical_dictionary = lookup_dictionary.medical_dictionary('static/medical_dictionary.csv')
-        enriched_diagnosis = lookup_dictionary.add_html_tags_to_text(diagnosis_doc, medical_dictionary.reduced_medical_dictionary_data(diagnosis_doc))
+        enriched_diagnosis = lookup_dictionary.add_html_tags_to_text(st.session_state['diagnosis_text'], medical_dictionary.reduced_medical_dictionary_data(st.session_state['diagnosis_text']))
 
         st.markdown(enriched_diagnosis, unsafe_allow_html=True)
 
